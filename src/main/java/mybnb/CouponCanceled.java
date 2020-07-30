96d53f9a8f62562f6a93a775bf55b4b7ebdc81e5
hyejinlee <hyejin7571@naver.com>
2020-07-30 21:02:15 +0900

    first commit

---
+++ src/main/java/mybnb/CouponCanceled.java
@@ -0,0 +1,51 @@
+package mybnb;
+
+public class CouponCanceled extends AbstractEvent {
+
+    private Long id;
+    private Long guestId;
+    private String couponCode;
+    private String status;
+    private Long bookId;
+
+    public CouponCanceled(){
+        super();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Long getGuestId() {
+        return guestId;
+    }
+
+    public void setGuestId(Long guestId) {
+        this.guestId = guestId;
+    }
+    public String getCouponCode() {
+        return couponCode;
+    }
+
+    public void setCouponCode(String couponCode) {
+        this.couponCode = couponCode;
+    }
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Long getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(Long bookId) {
+        this.bookId = bookId;
+    }
+}
